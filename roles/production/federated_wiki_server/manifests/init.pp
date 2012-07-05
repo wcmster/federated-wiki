7f506524bf860cc0ad4d4463d8729a02c95a22ca
wcmster <wcmster@gmail.com>
2012-07-05 17:59:58 +0000

    federated-wikiwas configured

--- roles/production/federated_wiki_server/manifests/init.pp
+++ roles/production/federated_wiki_server/manifests/init.pp
@@ -1,3 +1,9 @@
 class federated_wiki_server {
-	include 'federated_wiki'
+	class {
+		'federated_wiki' :
+			persistent_device => '/dev/sdh',
+			open_id_identifier => 'https://www.google.com/accounts/o8/id',
+			source_git_repository =>
+			'git://github.com/WardCunningham/Smallest-Federated-Wiki.git'
+	}
 }
